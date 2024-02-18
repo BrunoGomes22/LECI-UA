3d5c37eeb59f44d5ebfb5bb80b8fd152a1334f94
BrunoGomes22 <brunofgomes@ua.pt>
2024-02-18 18:01:05 +0000

    ex 2 adicional

--- 2_Ano/AC2/praticas2023_2024/aula01/parte2/ex2adi.s
+++ 2_Ano/AC2/praticas2023_2024/aula01/parte2/ex2adi.s
@@ -4,6 +4,7 @@
     .equ    PRINT_STR, 8
     .equ    PRINT_INT, 6
     .equ    PUT_CHAR, 3
+    .equ    PRINT_INT10, 7
 
     .data
 res:    .asciiz "\nResultados:\n"
@@ -14,6 +15,9 @@ str3:   .space 41
     .text
     .globl main
 main:
+    addiu $sp, $sp, -4
+    sw $ra, 0($sp)
+
     la $a0, str
     li $v0, PRINT_STR
     syscall
@@ -56,25 +60,37 @@ main:
     li $v0, PRINT_INT
     syscall # printInt(strlen(str2), 10)
 
+    li $a0, '\n'
+    li $v0, PUT_CHAR
+    syscall # putchar('\n')
+
     la $a0, str3
     la $a1, str1
     jal strcpy # strcpy(str3, str1)
 
     move $a0, $v0
     la $a1, str2
-    addiu $sp, $sp, -12
-    sw $ra, 0($sp)
-    sw $s0, 4($sp)
-    sw $s1, 8($sp)
+
     jal strcat # strcat(str3, str2)
-    lw $ra, 0($sp)
-    lw $s0, 4($sp)
-    lw $s1, 8($sp)
-    addiu $sp, $sp, 12
 
     move $a0, $v0
     li $v0, PRINT_STR
     syscall # printStr(str3)
+
+    la $a0, str1
+    la $a1, str2
+    jal strcmp # strcmp(str1, str2)
+
+    li $a0, '\n'
+    li $v0, PUT_CHAR
+    syscall # putchar('\n')
+
+    move $a0, $v0
+    li $v0, PRINT_INT10
+    syscall # printInt10(strcmp(str1, str2))
+
+    lw $ra, 0($sp)
+    addiu $sp, $sp, 4
 
     li $v0, 0
     jr $ra
@@ -110,6 +126,11 @@ endfor1:
 ########## funct strcat ###########
 
 strcat:
+    addiu $sp, $sp, -12
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+
     move $s0, $a0 # char *p = dst
     move $s1, $a0
 
@@ -118,9 +139,27 @@ for2: lb $t6, 0($s1) # for( ; *dst != '\0'; dst++ );
     addi $s1, $s1, 1
     j for2
 endfor2:
-    move $a0, $s1 # update $a0 to point to the end of the destination string
-    move $a1, $a1 # update $a1 to point to the source string
+    move $a0, $s1
     jal strcpy # strcpy(dst, src)
     move $v0, $s0
 
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    addiu $sp, $sp, 12
+
+    jr $ra
+
+########## funct strcmp ###########
+
+strcmp:
+for3: lb $t5, 0($a0)
+    lb $t6, 0($a1)
+    bne $t5, $t6, endfor3
+    beq $t5, 0, endfor3
+    addi $a0, $a0, 1
+    addi $a1, $a1, 1
+    j for3
+endfor3:
+    sub $v0, $t5, $t6
     jr $ra
