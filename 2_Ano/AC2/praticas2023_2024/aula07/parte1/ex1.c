380dc58c25cff80bd5a9b36b5b75683917e353d3
BrunoGomes22 <brunofgomes@ua.pt>
2024-08-01 17:05:57 +0100

    aula7 remake

--- 2_Ano/AC2/praticas2023_2024/aula07/parte1/ex1.c
+++ 2_Ano/AC2/praticas2023_2024/aula07/parte1/ex1.c
@@ -1,35 +1,46 @@
 #include <detpic32.h>
 
-int main(void) {
-    TRISBbits.TRISB4 = 1;   // RB4 digital output disconnected
-    AD1PCFGbits.PCFG4 = 0;  // RB4 configured as analog input (AN4)
-    AD1CON1bits.SSRC = 7;   // Conversion trigger constant
-    AD1CON1bits.CLRASAM = 1;  // Stop conversion when the 1st A/D converter intetupr is generated.
-            // At the same time, hardware clears ASAM bit
-    AD1CON3bits.SAMC = 16;  // Sample time is 16 TAD (TAD = 100ns)
-    AD1CON2bits.SMPI = 0;   // Interrupt is generated after 1 sample
-    AD1CHSbits.CH0SA = 4;   // analog channel input 4
-    AD1CON1bits.ON = 1;     // Enable the A/d configuration sequence
+void setup();
 
-    // Enable interrupts ADC
-    IPC6bits.AD1IP = 2;  // configure priority of A/D interrupts
-    IFS1bits.AD1IF = 0;  // clear A/D interrupt flag
-    IEC1bits.AD1IE = 1;  // enable A/D interrupts
-
-    EnableInterrupts();
-
-    AD1CON1bits.ASAM = 1;  // Start conversion
-
-    while (1) {
-    }
-
+int main(void){
+    setup();
+    EnableInterrupts();   //Global Interrupt Enable
+    AD1CON1bits.ASAM = 1; // Start conversion
+    while(1);             // all activity is done by the ISR
     return 0;
 }
 
-// Interrupt handler
-void _int_(27) isr_adc(void) {
-    printInt(ADC1BUF0, 16 | 3 << 16);
-    putChar('\r');
-    AD1CON1bits.ASAM = 1;  // Start conversion
-    IFS1bits.AD1IF = 0;
+
+void setup(){
+    TRISBbits.TRISB4 = 1;       // RBx digital output disconnected
+    AD1PCFGbits.PCFG4= 0;       // RBx configured as analog input
+    AD1CON1bits.SSRC = 7;       // Conversion trigger selection bits: in this
+                                // mode an internal counter ends sampling and
+                                // starts conversion
+    AD1CON1bits.CLRASAM = 1;    // Stop conversions when the 1st A/D converter
+                                // interrupt is generated. At the same time,
+                                // hardware clears the ASAM bit
+    AD1CON3bits.SAMC = 16;      // Sample time is 16 TAD (TAD = 100 ns)
+    AD1CON2bits.SMPI = 1-1;     // Interrupt is generated after N samples
+                                // (replace N by the desired number of
+                                // consecutive samples)
+    AD1CHSbits.CH0SA = 4;       // replace x by the desired input
+                                // analog channel (0 to 15)
+    AD1CON1bits.ON = 1;         // Enable A/D converter
+                                // This must the last command of the A/D
+                                // configuration sequence
+    IPC6bits.AD1IP = 2;         // configure priority of A/D interrupts
+    IFS1bits.AD1IF = 0;         // clear A/D interrupt flag
+    IEC1bits.AD1IE = 1;         // enable A/D interrupts
 }
+
+// Interrupt Handler
+void _int_(27) isr_adc(void)        //Replace VECTOR by the A/D vector
+                                    // number - see "PIC32 family data
+                                    // sheet" (pages 74-76)
+{
+    printInt(ADC1BUF0, 16 | 3 << 16);  // Read conversion result (ADC1BUF0) and print it
+    putChar('\r');
+    AD1CON1bits.ASAM = 1; // Start conversion
+    IFS1bits.AD1IF = 0;   // Reset AD1IF flag
+}                    
--- 2_Ano/AC2/praticas2023_2024/aula07/parte1/ex2.c
+++ 2_Ano/AC2/praticas2023_2024/aula07/parte1/ex2.c
@@ -1,92 +1,111 @@
 #include <detpic32.h>
-unsigned char toBcd(unsigned char value);
+
+void setup();
 void send2displays(unsigned char value);
+unsigned char toBCD(unsigned char value);
 void delay(int ms);
-volatile unsigned char voltage = 0;  // Global variable
 
-int main(void) {
-    unsigned int cnt = 0;
-    // Configure all (digital I/O, analog input, A/D module, interrupts)
-    TRISB = TRISB & 0x80FF;  // TRISB[8] A TRISB[14] SAO SAIDAS
-    TRISD = TRISD & 0xFF9F;  // displays high e low sao saidas
+volatile unsigned char voltage = 0; // Global variable
 
-    TRISBbits.TRISB4 = 1;   // RB4 digital output disconnected
-    AD1PCFGbits.PCFG4 = 0;  // RB4 configured as analog input (AN4)
-    AD1CON1bits.SSRC = 7;   // Conversion trigger constant
-    AD1CON1bits.CLRASAM =
-        1;  // Stop conversion when the 1st A/D converter intetupr is generated.
-            // At the same time, hardware clears ASAM bit
-    AD1CON3bits.SAMC = 16;  // Sample time is 16 TAD (TAD = 100ns)
-    AD1CON2bits.SMPI = 7;   // Interrupt is generated after 1 sample
-    AD1CHSbits.CH0SA = 4;   // analog channel input 4
-    AD1CON1bits.ON = 1;     // Enable the A/d configuration sequence
+int main(void){
+    setup();
+    int cnt = 0;
 
-    // Enable interrupts ADC
-    IPC6bits.AD1IP = 2;  // configure priority of A/D interrupts
-    IFS1bits.AD1IF = 0;  // clear A/D interrupt flag
-    IEC1bits.AD1IE = 1;  // enable A/D interrupts
+    TRISB = TRISB & 0x80FF; // RB8-RB14 configured as output
+    TRISDbits.TRISD5 = 0;
+    TRISDbits.TRISD6 = 0;
 
-    EnableInterrupts();  // Global Interrupt Enable
-
-    while (1) {
-        if (cnt % 20 == 0 && cnt != 0) {
-            AD1CON1bits.ASAM = 1;  // Start conversion
+    EnableInterrupts();   //Global Interrupt Enable
+    
+    while(1)             // all activity is done by the ISR
+    {
+        if(cnt == 0){ // 20*10ms (5HZ)
+            AD1CON1bits.ASAM = 1; // Start A/D conversion
         }
-        // Send "voltage" value to displays
-        send2displays(toBcd(voltage));
-        delay(10);
-        cnt++;
+
+        send2displays((toBCD(voltage))); // Send "voltage" value to displays
+        cnt = (cnt + 1) % 20; 
+        delay(10); // Wait 10 ms
     }
     return 0;
 }
 
-// Interrupt handler
-void _int_(27) isr_adc(void) {
-    int i, average = 0;
-    // Calculate buffer average (8 samples)
-    int *p = (int *)(&ADC1BUF0);
-    for (i = 0; i < 8; i++) {
-        average += (p[i * 4] * 33 + 511) / 1023;
+// Interrupt Handler
+void _int_(27) isr_adc(void)        //Replace VECTOR by the A/D vector
+                                    // number - see "PIC32 family data
+                                    // sheet" (pages 74-76)
+{
+    int i;
+    int sum = 0;
+
+    int *p = (int *)(&ADC1BUF0); // get adress of first buffer
+    for(i = 0; i<8; i++){
+        sum += (p[i*4]*33+511)/1023; // Calculate voltage amplitude
     }
-    // Calculate voltage amplitude
-    voltage = average / 8;
-    // Convert voltage amplitude to decimal and store the result in the global
-    // variable "voltage"
-    IFS1bits.AD1IF = 0;
+
+    voltage = sum/8; // Convert voltage amplitude to decimal and store the result in the
+                     //global variable "voltage"
+
+    IFS1bits.AD1IF = 0;   // Reset AD1IF flag
 }
 
-void send2displays(unsigned char value) {
-    static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D,
-                                          0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C,
-                                          0x39, 0x5E, 0x79, 0x71};
-    static char displayFlag = 0;
-    unsigned char dh = display7Scodes[value >> 4];
-    unsigned char dl = display7Scodes[value & 0x0F];
 
-    if (displayFlag == 0) {
+void setup(){
+    TRISBbits.TRISB4 = 1;       // RBx digital output disconnected
+    AD1PCFGbits.PCFG4= 0;       // RBx configured as analog input
+    AD1CON1bits.SSRC = 7;       // Conversion trigger selection bits: in this
+                                // mode an internal counter ends sampling and
+                                // starts conversion
+    AD1CON1bits.CLRASAM = 1;    // Stop conversions when the 1st A/D converter
+                                // interrupt is generated. At the same time,
+                                // hardware clears the ASAM bit
+    AD1CON3bits.SAMC = 16;      // Sample time is 16 TAD (TAD = 100 ns)
+    AD1CON2bits.SMPI = 8-1;     // Interrupt is generated after N samples
+                                // (replace N by the desired number of
+                                // consecutive samples)
+    AD1CHSbits.CH0SA = 4;       // replace x by the desired input
+                                // analog channel (0 to 15)
+    AD1CON1bits.ON = 1;         // Enable A/D converter
+                                // This must the last command of the A/D
+                                // configuration sequence
+    IPC6bits.AD1IP = 2;         // configure priority of A/D interrupts
+    IFS1bits.AD1IF = 0;         // clear A/D interrupt flag
+    IEC1bits.AD1IE = 1;         // enable A/D interrupts
+}
+
+void send2displays(unsigned char value)
+{
+    static const char disp7Scodes[] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
+    static char displayFlag = 0;
+
+    if(displayFlag == 0){
+        //select display low
+        LATDbits.LATD5 = 1;
+        LATDbits.LATD6 = 0;
+        // send digit_low (dl) to display: dl = value & 0x0F
+        LATB = (LATB & 0x80FF) | (disp7Scodes[value & 0x0F] << 8);
+    }
+    else{
+        //select display high
         LATDbits.LATD6 = 1;
         LATDbits.LATD5 = 0;
-        LATB = (LATB & 0x80FF) |
-               ((unsigned int)(dh))
-                   << 8;  // Clean the display and set the right value
-    } else {
-        LATDbits.LATD6 = 0;
-        LATDbits.LATD5 = 1;
-        LATB = (LATB & 0x80FF) |
-               ((unsigned int)(dl))
-                   << 8;  // Clean the display and set the right value
+        //send digit_high (dh) to display: dh = value >> 4
+        LATB = (LATB & 0x80FF) | (disp7Scodes[value >> 4] << 8);
     }
-    displayFlag = !displayFlag;
+
+    displayFlag = !displayFlag; //toggle displayFlag
+    
 }
 
-unsigned char toBcd(unsigned char value) {
-    return ((value / 10) << 4) + (value % 10);
+unsigned char toBCD(unsigned char value){
+    return ((value/10) << 4) + (value %10);
 }
 
-void delay(int ms) {
+void delay(int ms)
+{
     resetCoreTimer();
     int timer = readCoreTimer();
-    while (timer < ms * 20000) {
+    while(timer < ms * 20000){
         timer = readCoreTimer();
     }
-}
+} 
