12e447afa5123367b5f934ac2e8ce830810349e5
BrunoGomes22 <brunofgomes@ua.pt>
2024-08-04 18:37:16 +0100

    ex2 part2 incomplete

---
+++ 2_Ano/AC2/praticas2023_2024/aula08/parte2/ex2.c
@@ -0,0 +1,36 @@
+#include <detpic32.h>
+
+void delay(int ms);
+
+volatile int counter = 0;
+
+int main(void){
+    TRISEbits.TRISE0 = 0; // RE0 configured as output
+    TRISDbits.TRISD8 = 1; // RD8 configured as input
+
+    // Timer 2 Configuration
+    T2CONbits.TCKPS = 7;    // 1:256 prescaler
+    PR2 = 39061;            
+    TMR2 = 0;               // Clear timer T2 count register
+    T2CONbits.TON = 1;      // Enable timer T2 (must be the last command of the
+                            // timer configuration sequence)
+
+    IPC2bits.T2IP = 2; // Interrupt priority (must be in range [1..6])
+    IEC0bits.T2IE = 1; // Enable timer T2 interrupts
+    IFS0bits.T2IF = 0; // Reset timer T2 interrupt flag
+
+    EnableInterrupts();
+
+    while(1);
+
+    return 0;
+
+}
+
+void _int_(8) isr_T2(void){
+
+}
+
+void _int_(7) isr_INT1(void){
+
+}
