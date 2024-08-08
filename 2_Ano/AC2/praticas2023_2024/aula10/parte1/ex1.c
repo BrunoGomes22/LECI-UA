9f7cdb63c39484a2f216da8d9656ff92332ad87b
BrunoGomes22 <brunofgomes@ua.pt>
2024-08-08 23:22:31 +0100

    ex1 aula 10

--- 2_Ano/AC2/praticas2023_2024/aula10/parte1/ex1.c
+++ 2_Ano/AC2/praticas2023_2024/aula10/parte1/ex1.c
@@ -1,13 +1,17 @@
 #include <detpic32.h>
 
-int main(void) {
-    // Configure UART2
-    U2BRG = 10;               // U2BRG = (20Mhz / (16*115200))-1 ~ 10
-    U2MODEbits.PDSEL = 0b00;  // 00 = 8-bit data, no parity
-    U2MODEbits.STSEL = 0;     // only 1 stop bit
-    U2MODEbits.BRGH = 0;      // divide by 16
-    U2STAbits.URXEN = 1;      // Enable Receiver
-    U2STAbits.UTXEN = 1;      // Enable Transmitter
-    U2MODEbits.ON = 1;        // Enable UART2
+int main(void){
+    // Configure UART2:
+    // 1 - Configure BaudRate Generator
+    U2BRG = 10;
+    U2BRG.BRGH = 0; // fator divisao 16
+    // 2 - Configure number of data bits, parity and number of stop bits
+    U2MODEbits.PDSEL = 0b00; // 8 bit data, no parity
+    U2MODEbits.STSEL = 0;    // 1 stop bit
+    // 3 - Enable the transmitter and receiver modules
+    U2STAbits.UTXEN = 1; // enable transmitter
+    U2STAbits.URXEN = 1; // enable receiver
+    // 4 - Enable UART2
+    U2MODEbits.ON = 1;
     return 0;
 }
