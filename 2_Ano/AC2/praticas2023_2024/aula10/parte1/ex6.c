776cdaedcfc9318065019390954dd032e7958995
BrunoGomes22 <brunofgomes@ua.pt>
2024-08-10 23:10:38 +0100

    ex6 parte2 aula10

--- 2_Ano/AC2/praticas2023_2024/aula10/parte1/ex6.c
+++ 2_Ano/AC2/praticas2023_2024/aula10/parte1/ex6.c
@@ -1,7 +1,11 @@
 #include <detpic32.h>
 
-int main(void){
-    // Configure UART2:
+char getc(void);
+void putc(char byte);
+
+int main(void)
+{
+    // Configure UART2 (115200, N, 8, 1)
     // 1 - Configure BaudRate Generator
     U2BRG = 10;
     U2MODEbits.BRGH = 0; // fator divisao 16
@@ -13,5 +17,21 @@ int main(void){
     U2STAbits.URXEN = 1; // enable receiver
     // 4 - Enable UART2
     U2MODEbits.ON = 1;
+    while(1)
+    {
+        putc(getc());// Read character using getc() and send character using putc()
+    }
     return 0;
+}
+
+char getc(void)
+{
+    while(U2STAbits.URXDA == 0); // Wait while URXDA == 0
+    return U2RXREG; // Return U2RXREG
+}
+
+void putc(char byte)
+{
+    while(U2STAbits.UTXBF == 1); // wait while UTXBF == 1
+    U2TXREG = byte;              // Copy "byte" to the U2TXREG register
 }
