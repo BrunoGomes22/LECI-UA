859de1be8516266998d3c494f98d3d947dd82e43
BrunoGomes22 <brunofgomes@ua.pt>
2024-08-15 23:11:15 +0100

    aula11 parte2 half done

--- 2_Ano/AC2/praticas2023_2024/aula11/parte2/buffer.h
+++ 2_Ano/AC2/praticas2023_2024/aula11/parte2/buffer.h
@@ -1,6 +1,6 @@
-typedef struct 
- { 
-  char mem[100]; // Storage area 
-  int nchar;   // Number of characters to be transmitted 
-  int posrd;   // Position of the next character to be transmitted 
- } t_buf; 
+typedef struct buffer
+{
+    char mem[100]; // Storage area
+    int nchar;     // Number of characters to be transmitted
+    int posrd;     // Position of the next character to be transmitted
+} t_buf;
--- 2_Ano/AC2/praticas2023_2024/aula11/parte2/ex1.c
+++ 2_Ano/AC2/praticas2023_2024/aula11/parte2/ex1.c
@@ -1,63 +1,37 @@
 #include <detpic32.h>
-#include </home/brunogomes/AC2/praticas2023_2024/aula11/parte2/buffer.h>
-void putstrInt(char *s);
+#include </home/brunogomes/AC2/estudo_ep_especial/aula11/parte2/buffer.h>
 
 volatile t_buf txbuf;
-int main(void) {
-    // Configure UART2: 115200, N, 8, 1
-    U2BRG = 10;               // U2BRG = (20Mhz / (16*115200))-1 ~ 10
-    U2MODEbits.PDSEL = 0b00;  // 00 = 8-bit data, no parity
-    U2MODEbits.STSEL = 0;     // only 1 stop bit
-    U2MODEbits.BRGH = 0;      // divide by 16
 
-    U2STAbits.URXEN = 1;       // Enable Receiver
-    U2STAbits.UTXEN = 1;       // Enable Transmitter
-    U2STAbits.UTXISEL = 0b00;  // Select interrupt only for receiver
-    IPC8bits.U2IP = 1;         // UART2 Priority
-    IEC1bits.U2RXIE = 0;       // U2RX Interrupt Enable
-    IEC1bits.U2TXIE = 0;       // U2TX Interrupt Disable
-    IFS1bits.U2RXIF = 0;       // U2RX Interrupt Flag
-    TRISCbits.TRISC14 = 0;     // saida
-    U2MODEbits.ON = 1;         // Enable UART2
-    // Enable global Interrupts
-    txbuf.nchar = 0;
-    EnableInterrupts();
-    while (1) {
-        putstrInt(
-            "Test string which can be as long as you like as long as it is no "
-            "longer than 100 characters\n");
-    }
-    return 0;
+int main(void){
+
 }
 
-void putstrInt(char *s) {
-    while (txbuf.nchar > 0);
-    // Wait while the buffer is not empty
-    // Copy all characters of the string "s" to the buffer
-    int i;
-    for (i = 0; s[i] != '\0'; i++) {
-        txbuf.mem[i] = s[i];
+void putstrInt(char *s)
+{   
+    int i = 0;
+    while(txbuf.nchar > 0); // wait while the buffer is not empty
+
+    while(s[i] != '\0'){
+        txbuf.mem[i] = s[i++];
         txbuf.nchar++;
     }
-    // Initialize "posrd" variable with 0
-    txbuf.posrd = 0;
-    // Enable UART2 Tx interrupts
-    IEC1bits.U2TXIE = 1;  // U2TX Interrupt Enable
+    txbuf.posrd = 0;  // Initialize "posrd" variable with 0
+    IEC1bits.U2TXIE = 1;  // Enable UART2 Tx interrupts
 }
 
-void _int_(32) isr_uart2(void) {
-    if (IFS1bits.U2TXIF == 1) {
-        if (txbuf.nchar > 0) {  // At least one character to be transmitted
-            // U2TXREG = ... // Read 1 character from the buffer and
-            U2TXREG = txbuf.mem[txbuf.posrd];
-            // Update buffer "posrd" and "nchar" variables
+void _int_(32) isr_uart2(void) // 32 is the vector number of UART2
+{
+    if(IFS1bits.U2TXIF = 1){ // UART2 Tx interrupt flag is set
+
+        if(txbuf.nchar > 0){
+            U2TXREG = txbuf.mem[txbuf.posrd]; // Read 1 character from the buffer and send it
             txbuf.posrd++;
             txbuf.nchar--;
-        } else {
-            // Disable UART2 Tx interrupts
-            IEC1bits.U2TXIE = 0;
         }
-        // Clear UART2 Tx interrupt flag
-        IFS1bits.U2TXIF = 0;  // U2TX Interrupt Flag
+        else{
+            IEC1bits.U2TXIE = 0;  // Disable UART2 Tx interrupts
+        }
+        IFS1bits.U2TXIF = 0; // Clear UART2 Tx interrupt flag
     }
 }
