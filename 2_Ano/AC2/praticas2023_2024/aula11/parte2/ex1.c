a3e103e6224d0076adc8c89bb78536f10aa24a27
BrunoGomes22 <brunofgomes@ua.pt>
2024-08-16 10:01:10 +0100

    aula11 parte2 done

--- 2_Ano/AC2/praticas2023_2024/aula11/parte2/ex1.c
+++ 2_Ano/AC2/praticas2023_2024/aula11/parte2/ex1.c
@@ -1,10 +1,40 @@
 #include <detpic32.h>
 #include </home/brunogomes/AC2/estudo_ep_especial/aula11/parte2/buffer.h>
 
+void putstrInt(char *s);
+
 volatile t_buf txbuf;
 
 int main(void){
+    // Configure UART2: 115200,N,8,1
+    // 1 - Configure BaudRate Generator
+    U2BRG = 10;
+    U2MODEbits.BRGH = 0; // fator divisao 16
+    // 2 - Configure number of data bits, parity and number of stop bits
+    U2MODEbits.PDSEL = 0b00; // 8 bit data, no parity
+    U2MODEbits.STSEL = 0;    // 1 stop bit
+    // 3 - Enable the transmitter and receiver modules
+    U2STAbits.UTXEN = 1; // enable transmitter
+    U2STAbits.URXEN = 1; // enable receiver
+    // 4 - Enable UART2
+    U2MODEbits.ON = 1;
 
+    // Configure UART2 interrupts, with RX interrupts disable and TX interrupts disabled
+    IEC1bits.U2RXIE = 0; // enable U2RXIE
+    IEC1bits.U2TXIE = 0; // disable U2TXIE
+    IPC8bits.U2IP = 1; // set UART2 priority level
+    IFS1bits.U2RXIF = 0; // clear interrupt flag bit
+    U2STAbits.URXISEL = 0b00; // define RX interrupt mode
+
+    EnableInterrupts();
+
+    txbuf.nchar = 0;
+
+    while(1)
+    {
+        putstrInt("Test string which can be as long as you like, up to a maximum of 100 characters\n");  
+    }
+    return 0;
 }
 
 void putstrInt(char *s)
